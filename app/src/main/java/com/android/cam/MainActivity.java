a4273971f09c80b084236231862400019b89716b
habib30 <habibhussein30@gmail.com>
2020-04-01 10:12:00 +0300

    Initial commit

---
+++ app/src/main/java/com/android/cam/MainActivity.java
@@ -0,0 +1,86 @@
+package com.android.cam;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.os.Environment.getExternalStoragePublicDirectory;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnTakePic;
+    ImageView imageView;
+    String pathToFile;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnTakePic = findViewById(R.id.btnTakePic);
+        if (Build.VERSION.SDK_INT>=23){
+            requestPermissions(new String[]{"Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE"},2);
+        }
+        btnTakePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchPictureTakerAction();
+            }
+        });
+        imageView = findViewById(R.id.image);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        if (resultCode == RESULT_OK){
+            if (requestCode == 1){
+                Bitmap bitmap = BitmapFactory.decodeFile(pathToFile);
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+    }
+
+    private void dispatchPictureTakerAction() {
+        Intent takePic = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePic.resolveActivity(getPackageManager()) !=null){
+            File photoFile = null;
+            photoFile = createPhotoFile();
+
+            if (photoFile!=null) {
+                photoFile.getAbsolutePath();
+                Uri photoURI = FileProvider.getUriForFile(MainActivity.this,"com.thecodecity.cameraandroid.fileprovider",photoFile);
+                takePic.putExtra(MediaStore.EXTRA_OUTPUT,photoURI);
+                startActivityForResult(takePic, 1);
+            }
+        }
+    }
+
+    private File createPhotoFile() {
+        String name = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File storageDir = getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = null;
+        try {
+            image = File.createTempFile(name, ".jpg", storageDir);
+        } catch (IOException e) {
+            Log.d("my log", "Excep : " + e.toString());
+        }
+        return image;
+    }
+}
